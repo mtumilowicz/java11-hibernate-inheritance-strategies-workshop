5b759aa97011bb288f9045e500c72b8a120a0202
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-16 23:24:36 +0100

    ShapeServiceTest.groovy

---
+++ src/test/groovy/app/singletable/ShapeServiceTest.groovy
@@ -0,0 +1,53 @@
+package app.singletable
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ShapeServiceTest extends Specification {
+
+    @Autowired
+    ShapeService shapeService
+
+    def setup() {
+        saveCircles()
+        saveRectangles()
+    }
+
+    def saveCircles() {
+        def circle = new Circle()
+        circle.radius = 15
+        shapeService.save(circle)
+    }
+
+    def saveRectangles() {
+        def rectangle = new Rectangle()
+        rectangle.length = 10
+        rectangle.width = 25
+        shapeService.save(rectangle)
+    }
+
+    def cleanup() {
+        shapeService.deleteAll()
+    }
+
+    def "FindAllCircles"() {
+        expect:
+        shapeService.findAllCircles().radius == [15]
+    }
+
+    def "FindAllRectangles"() {
+        given:
+        def rectangles = shapeService.findAllRectangles()
+
+        expect:
+        rectangles.length == [10]
+        rectangles.width == [25]
+    }
+
+    def "FindAll"() {
+        expect:
+        shapeService.findAll().size() == 2
+    }
+}
