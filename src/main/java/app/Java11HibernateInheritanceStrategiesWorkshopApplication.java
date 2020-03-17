1b533bc72aae6f42bc37926e4bcd49246fae35be
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-17 16:21:11 +0100

    removing comments

--- src/main/java/app/Java11HibernateInheritanceStrategiesWorkshopApplication.java
+++ src/main/java/app/Java11HibernateInheritanceStrategiesWorkshopApplication.java
@@ -10,42 +10,4 @@ public class Java11HibernateInheritanceStrategiesWorkshopApplication {
         SpringApplication.run(Java11HibernateInheritanceStrategiesWorkshopApplication.class, args);
     }
 
-//    @Bean
-//    public ApplicationRunner demoData(TagRepository tagRepository,
-//                                      RectangleRepository rectangleRepository,
-//                                      CircleRepository circleRepository,
-//                                      PetRepository petRepository,
-//                                      WildRepository wildRepository,
-//                                      EmployeeRepository employeeRepository,
-//                                      EmployerRepository employerRepository) {
-//        return args -> {
-//            tagRepository.save(Tag.builder().name("tag1").build());
-//
-//            Circle circle = new Circle();
-//            circle.setRadius(40);
-//            circleRepository.save(circle);
-//
-//            Rectangle rectangle = new Rectangle();
-//            rectangle.setLength(10);
-//            rectangle.setWidth(30);
-//            rectangleRepository.save(rectangle);
-//
-//            Pet pet = new Pet();
-//            pet.setName("pet");
-//            petRepository.save(pet);
-//
-//            Wild wild = new Wild();
-//            wild.setEndangered(true);
-//            wildRepository.save(wild);
-//
-//            Employee employee = new Employee();
-//            employee.setSalary(1000);
-//            employeeRepository.save(employee);
-//
-//            Employer employer = new Employer();
-//            employer.setBudget(1_000_000);
-//            employerRepository.save(employer);
-//        };
-//    }
-
 }
